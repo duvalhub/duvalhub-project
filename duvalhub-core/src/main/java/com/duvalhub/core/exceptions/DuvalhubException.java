29ccdf4c98c6f0196013c4d5f83b9017e4ff3e16
Maxthod <47720660+Maxthod@users.noreply.github.com>
2022-02-21 00:43:35 -0500

    Initial (#1)
    * "initial"

---
+++ duvalhub-core/src/main/java/com/duvalhub/core/exceptions/DuvalhubException.java
@@ -0,0 +1,7 @@
+package com.duvalhub.core.exceptions;
+
+public class DuvalhubException extends RuntimeException {
+    public DuvalhubException() {
+        super("Duvalhub Exception occurred");
+    }
+}
