2dd0e0c64bb895ed8f3d12f2855c0aa950b25667
Hugues McNeil-Duval <hugues.mcneil-duval@duvalhub.com>
2024-02-19 13:26:32 -0500

    Upgrade to spring-boot 3.2.2 + migrate from javax to jakarta + upgraded spring-cloud to 4.1.0

--- duvalhub-google/src/main/java/com/duvalhub/google/GoogleCredentialsProperties.java
+++ duvalhub-google/src/main/java/com/duvalhub/google/GoogleCredentialsProperties.java
@@ -1,10 +1,10 @@
 package com.duvalhub.google;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotEmpty;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
--- duvalhub-google/src/main/java/com/duvalhub/google/GoogleProperties.java
+++ duvalhub-google/src/main/java/com/duvalhub/google/GoogleProperties.java
@@ -1,12 +1,12 @@
 package com.duvalhub.google;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotEmpty;
 
 @Configuration
 @ConfigurationProperties("google")
